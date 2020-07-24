9b1697a70cb2b6d93abdc3847418f22137e12ed3
bajwauto <bajwauto@gmail.com>
2020-07-25 00:25:47 +0530

    Improved error handling in data provider class

--- src/test/java/expedia/Data.java
+++ src/test/java/expedia/Data.java
@@ -1,12 +1,13 @@
 package expedia;
 
+import static log.Log.error;
 import static log.Log.info;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
+import org.testng.Assert;
 import org.testng.ITestNGMethod;
 import org.testng.annotations.DataProvider;
 
@@ -16,15 +17,22 @@ import utils.Utility;
 public class Data {
 
 	@DataProvider(name = "excel")
-	public Object[][] getDataFromExcel(ITestNGMethod method) throws IOException {
+	public Object[][] getDataFromExcel(ITestNGMethod method) {
 		String filePath = Utility.getAbsoluteProjectPaths("testData") + File.separator + method.getMethodName()
 				+ ".xlsx";
 		info("Reading test data from the excel file at locaton - " + filePath);
 		Excel excel = new Excel(filePath, "Data");
-		List<Map<String, Object>> excelData = excel.read();
-		Object[][] testData = new Object[excelData.size()][1];
-		for (int i = 0; i < excelData.size(); i++)
-			testData[i][0] = excelData.get(i);
+		List<Map<String, Object>> excelData;
+		Object[][] testData = null;
+		try {
+			excelData = excel.read();
+			testData = new Object[excelData.size()][1];
+			for (int i = 0; i < excelData.size(); i++)
+				testData[i][0] = excelData.get(i);
+		} catch (Exception e) {
+			error("Test data file not found at path - " + filePath);
+			Assert.fail("Test data file not found at path - " + filePath);
+		}
 		return testData;
 	}
 }
